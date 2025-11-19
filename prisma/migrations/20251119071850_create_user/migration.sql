cb0fcdace4b84dc774df8196512c9003cd2abf2f
Raya Sabari <rayavexed@gmail.com>
2025-11-19 20:02:14 +0700

    feat: update user model

--- prisma/migrations/20251119071850_create_user/migration.sql
+++ prisma/migrations/20251119071850_create_user/migration.sql
@@ -3,9 +3,16 @@ CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "username" VARCHAR(255) NOT NULL,
     "displayName" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255),
     "email" VARCHAR(255) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
+ 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
